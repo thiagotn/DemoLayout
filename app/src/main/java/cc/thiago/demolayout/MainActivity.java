b202cdcbe9e88c4a21e149232ec256c7d8d3926b
thiagotn <thiagotn@gmail.com>
2015-11-12 23:55:24 -0200

    Primeiro commit

---
+++ app/src/main/java/cc/thiago/demolayout/MainActivity.java
@@ -0,0 +1,33 @@
+package cc.thiago.demolayout;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etUsuario;
+    private EditText etSenha;
+    private Button btAcessar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etUsuario = (EditText) findViewById(R.id.et_usuario);
+        etSenha = (EditText) findViewById(R.id.et_senha);
+        btAcessar = (Button) findViewById(R.id.bt_acessar);
+
+        btAcessar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, CadastroActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
